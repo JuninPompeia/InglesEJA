a9cbd6067729ed346205734f44318ef733b2db24
Pompeia <juninpompeia.fi@gmail.com>
2026-05-13 13:54:50 -0300

    Adicionada Tela de Quiz e Função de aleatorizar ordem das respostas

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function ordemQuestoes() {
+    const ordem = [0,0,0,0];
+    for (let i = 0; i < 4; i++){
+        let novoValor;
+        do {
+            novoValor = Math.trunc(Math.random() * (4 - 1 + 1) + 1);
+        } while (novoValor == ordem[0] || novoValor == ordem[1] || novoValor == ordem[2] || novoValor == ordem[3]);
+        ordem[i] = novoValor;
+    }
+    return ordem;
+}
