34d9d74950dffa2998561f0fb068b37cf90b45a6
Pompeia <juninpompeia.fi@gmail.com>
2026-05-14 13:10:25 -0300

    14/05 13:10

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function ordemQuestoes() {
     for (let i = 0; i < 4; i++){
         let novoValor;
         do {
-            novoValor = Math.trunc(Math.random() * (4 - 1 + 1) + 1);
+            novoValor = Math.trunc(Math.random() * (4 - 1 + 1)) + 1;
         } while (novoValor == ordem[0] || novoValor == ordem[1] || novoValor == ordem[2] || novoValor == ordem[3]);
         ordem[i] = novoValor;
     }
